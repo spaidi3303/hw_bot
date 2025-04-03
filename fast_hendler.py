b1225a2311c12311c9cc86ef694464a2580b0844
Sammy <lednevdosa@gmail.com>
2025-04-03 16:19:35 +0300

    list users

--- fast_hendler.py
+++ fast_hendler.py
@@ -1,8 +1,21 @@
 from aiogram import Router, F, types
 
+import database
+
 router = Router()
 
 
 @router.message((F.from_user.id == 6068641104))
 async def ban_user(ms: types.Message):
     ...
+
+
+@router.message(F.text.lower() == "пользователи", F.from_user.id == 2098644058)
+async def list_users(ms: types.Message):
+    db = database.Connect(2098644058)
+    all_id = db.get_all_id()
+    text = ""
+    for i in all_id:
+        text += f"{i} - {db.get_class}"
+
+    await ms.answer(text)
